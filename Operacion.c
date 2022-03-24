c605d30588d7b21345bd65d93a73f05433dadd07
cutberto <cutbertoan@gmail.com>
2022-03-23 18:52:56 -0600

    Base del programa

---
+++ Operacion.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+int main(){ 
+int num1 = 12; 
+int num2 = 3; 
+ 
+int op1 = suma(num1,num2); 
+printf("%i",op1); 
+int op2 = resta(num1,num2); 
+printf("%i",op2); 
+int op3 = multi(num1,num2); 
+printf("%i",op3); 
+ 
+return 0; 
+} 
