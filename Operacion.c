606bf7a7cc5f5349f6dea87426f4e22c01906d96
Jesus <A01706335@itesm.mx>
2022-03-23 19:05:29 -0600

    Codigo depurado

--- Operacion.c
+++ Operacion.c
@@ -8,9 +8,7 @@ int num2 = 3;
 int op1 = suma(num1,num2); 
 printf("%i",op1); 
 int op2 = resta(num1,num2); 
-printf("%i",op2); 
-int op3 = multi(num1,num2); 
-printf("%i",op3); 
+printf("%i",op2);
  
 return 0; 
 } 
